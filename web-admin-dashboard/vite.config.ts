d0bd19200419675e5fe892596bc9f8d71b9540cb
dylen chew <56499861+rgonslayer@users.noreply.github.com>
2023-11-13 13:42:15 +0800

    Update vite.config.ts

--- web-admin-dashboard/vite.config.ts
+++ web-admin-dashboard/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/ay2324s1-course-assessment-g18/",
   build: {
     outDir: "./dist",
   },
